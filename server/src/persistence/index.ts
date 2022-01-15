8792f2f365c62740a24318cf9bd9b2f6dd26f47f
agoemans <kewlboontjies@gmail.com>
2022-01-15 13:22:08 +0100

    Removed unnecessary code

--- server/src/persistence/index.ts
+++ server/src/persistence/index.ts
@@ -4,10 +4,6 @@ import { MySqlClient  } from './my-sql';
 import { InMemory } from './in-memory';
 
 export const createRepo = () => {
-    // if (process.env.NODE_ENV !== 'test') {
-    //     return new DynamoClient();
-    // }
-
     // return new MySqlClient();
 
     return new InMemory();
